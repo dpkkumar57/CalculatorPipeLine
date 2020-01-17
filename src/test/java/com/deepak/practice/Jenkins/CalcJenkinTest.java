55a417a78f6f29738d895e570df6686bc7a722f0
Deepak <dpkkumar57@gmail.com>
2020-01-16 19:56:43 -0600

    pipeline set up

---
+++ src/test/java/com/deepak/practice/Jenkins/CalcJenkinTest.java
@@ -0,0 +1,21 @@
+package com.deepak.practice.Jenkins;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+import com.deepak.practice.Jenkins.*;
+
+public class CalcJenkinTest {
+
+	@Test
+	public void addTest() {
+		Calculator calc = new Calculator();
+		assertEquals(10,calc.add(5, 5));
+	}
+	
+	@Test
+	public void subTest() {
+		Calculator calc = new Calculator();
+		assertEquals(2, calc.sub(6, 4));
+	}
+}
